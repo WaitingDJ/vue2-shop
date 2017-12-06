a8bad95d87c4e97aaeb3dfeda191b6a205718173
waitingdj <1174531170@qq.com>
2017-12-06 21:04:05 +0800

    GoodsList组建拆分，修正HelloWorld

---
+++ src/components/NavBread.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <div class="nav-breadcrumb-wrap">
+        <div class="container">
+          <nav class="nav-breadcrumb">
+            <a href="/">主页</a>
+            <span>商品列表</span>
+          </nav>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavBread',
+        data () {
+            return {
+                msg: 'Welcome to Your GoodsList'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GoodsList',
+      component: GoodsList
     },
     {
       path: '/GoodsList',
