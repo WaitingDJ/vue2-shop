e329b6be7831937a052046b34754443eea09bcbb
waitingdj <1174531170@qq.com>
2017-12-08 19:30:23 +0800

    服务端读取数据，并发送

--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('用户路由下的内容');
-});
-
-module.exports = router;
