33588e7525ee2a3d9ddcf3134677e6da133fc6d0
waitingdj <1174531170@qq.com>
2017-12-09 17:43:45 +0800

    页面排序功能的实现

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -7,8 +7,8 @@
       <div class="container">
         <div class="filter-nav">
           <span class="sortby">排序:</span>
-          <a href="javascript:void(0)" class="default cur">默认</a>
-          <a href="javascript:void(0)" class="price">价格 <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" class="default cur" @click="defaultSort()">默认</a>
+          <a href="javascript:void(0)" class="price" v-bind:class="{'sort-up':sortFlag}" @click="sortGoods()">价格 <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
           <a href="javascript:void(0)" class="filterby" @click.stop="showFilterPop">筛选</a>
         </div>
         <div class="accessory-result">
@@ -68,7 +68,7 @@
         goodsList: [],
         page: 1,
         pageSize: 8,
-        sortFlag: 1,
+        sortFlag: true,
         priceFilter: [
           {
             startPrice:'0.00',
@@ -113,15 +113,25 @@
         var param = {
           page: this.page,
           pageSize: this.pageSize,
-          sort: this.sortFlag
+          sort: this.sortFlag?1:-1
         }
-        
+
         axios.get("/goods",{
           params: param
         }).then((result) => {
           var res = result.data.result
           this.goodsList = res.list
         })
+      },
+      defaultSort(){
+        this.sortFlag = true;
+        this.page = 1;
+        this.getGoodsList();
+      },
+      sortGoods(){
+        this.sortFlag = !this.sortFlag;
+        this.page = 1;
+        this.getGoodsList();
       },
       setPriceFilter(index){
         this.priceChecked = index;
