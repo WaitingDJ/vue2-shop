591ca0109a312a35e1940d50d1800bb682a17f85
waitingdj <1174531170@qq.com>
2017-12-12 18:35:26 +0800

    登录拦截白名单

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -20,7 +20,7 @@ mongoose.connection.on('disconected', function () {
 
 
 //查询商品列表
-router.get('/', function(req, res, next) {
+router.get('/list', function(req, res, next) {
   let page = parseInt(req.param("page"));
   let pageSize = parseInt(req.param("pageSize"));
   let sort = req.param("sort");
