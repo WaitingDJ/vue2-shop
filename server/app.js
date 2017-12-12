a02af294c4e6f4f709d7fa4c155b0a0b8100c502
waitingdj <1174531170@qq.com>
2017-12-12 18:35:08 +0800

    登录拦截白名单

--- server/app.js
+++ server/app.js
@@ -25,31 +25,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-app.all('*', function (req,res,next) {
-  //CORS配置
+app.all('*', function(req, res, next) {
+  // CORS配置
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", '3.2.1');
-  res.header('Content-Type', "application/json;charset=utf-8");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
   next();
-
 });
-*/
 
-app.use(function (req, res, next) {
-  if(req.cookie.userId){
+
+app.use(function (req,res,next) {
+  if(req.cookies.userId){
     next();
   }else{
-    console.log("url:" + req.originalUrl);
-    if(req.originalUrl == '/users/login' || req.originalUrl == '/users/logout'){
+    console.log("url:"+req.originalUrl);
+    if(req.originalUrl=='/users/login' || req.originalUrl=='/users/logout' || req.originalUrl.indexOf('/goods/list')>-1){
       next();
     }else{
       res.json({
         status:'10001',
-        msg: '当前未登录',
-        result: ''
+        msg:'当前未登录',
+        result:''
       });
     }
   }
