fadd33ed46f68803073243ba6753af3803f8890e
waitingdj <1174531170@qq.com>
2017-12-16 04:11:04 +0800

    初始化订单成功页面，拆分订单成功页面， 挂载OrderSuccess

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 import Address from '@/views/Address'
 import OrderConfirm from '@/views/OrderConfirm'
+import OrderSuccess from '@/views/OrderSuccess'
 
 Vue.use(Router)
 
@@ -34,6 +35,11 @@ export default new Router({
       path: '/OrderConfirm',
       name: 'OrderConfirm',
       component: OrderConfirm
+    },
+    {
+      path: '/OrderSuccess',
+      name: 'OrderSuccess',
+      component: OrderSuccess
     }
   ]
 })
