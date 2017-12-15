a5e441aa61ccac8b0f97e97c1766d10fadc14359
waitingdj <1174531170@qq.com>
2017-12-16 03:38:43 +0800

    添加Data.format工具

---
+++ server/util/format.js
@@ -0,0 +1,31 @@
+Date.prototype.Format = function (fmt) {
+  var o = {
+    "M+": this.getMonth() + 1, //月份
+    "d+": this.getDate(), //日
+    "h+": this.getHours(), //小时
+    "m+": this.getMinutes(), //分
+    "s+": this.getSeconds(), //秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+    "S": this.getMilliseconds() //毫秒
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+}
+
+module.exports = {};
+
+vue2-shop-lesson\src\views\OrderConfirm.vue
+payMent(){
+  var addressId = this.$route.query.addressId;
+  axios.post("/users/payMent",{
+    addressId:addressId,
+    orderTotal:this.orderTotal
+  }).then((response)=>{
+    let res = response.data;
+    if(res.status=="0"){
+      alert('成功')
+    }
+  })
+}
